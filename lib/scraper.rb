d30c08ae6580644d1b593fd7aa9fc7e4a8c25ecb
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-03-02 20:41:24 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -11,6 +11,9 @@ class Scraper
     doc.css(".post").each do |post|
       course = Course.new 
       course.title = post.css("h2").text 
+      course.schedule = post.css(".date").text 
+      course.description = post.css("p").text 
+    end
       
       
     end
