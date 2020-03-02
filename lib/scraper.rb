1c05c7dcf53556e6a61ce8755ed35111a2870a2f
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-03-02 20:27:00 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,7 +7,8 @@ require_relative './course.rb'
 class Scraper
   
   def get_page
-    doc = Nokogiri::HTML(open())
+    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+    biding.pry
   end
   
   def print_courses
